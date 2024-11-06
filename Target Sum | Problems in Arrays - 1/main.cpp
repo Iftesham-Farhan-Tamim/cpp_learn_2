01d6ec0bf5e8ea973692c986531f90090586c98e
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-11-07 02:47:11 +0600

    second commit

--- Target Sum | Problems in Arrays - 1/main.cpp
+++ Target Sum | Problems in Arrays - 1/main.cpp
@@ -78,43 +78,131 @@
 // 4
 // ............................................................................................
 // //Find the second largest element in the given array.
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int secondLargestElement(int arr[], int size) {
-    int max = INT_MIN;
-    int second_max = INT_MIN;
+// int secondLargestElement(int arr[], int size) {
+//     int max = INT_MIN;
+//     int second_max = INT_MIN;
 
-    for(int i=0; i<size; i++) {
-        if(arr[i] > max) {
-            max=arr[i];
-        }
-    }
+//     for(int i=0; i<size; i++) {
+//         if(arr[i] > max) {
+//             max=arr[i];
+//         }
+//     }
 
-    for(int i=0; i<size; i++) {
-        if(arr[i] > second_max && arr[i]!=max) {
-            second_max=arr[i];
-        }
-    }
+//     for(int i=0; i<size; i++) {
+//         if(arr[i] > second_max && arr[i]!=max) {
+//             second_max=arr[i];
+//         }
+//     }
 
-    return second_max;
-}
+//     return second_max;
+// }
 
-int main() {
-    int arr[] = {2,3,5,7,6,1,7};
-    int n = 7;
+// int main() {
+//     int arr[] = {2,3,5,7,6,1,7};
+//     int n = 7;
 
-    cout << secondLargestElement(arr, n) << endl;
-}
+//     cout << secondLargestElement(arr, n) << endl;
+// }
 
 // output:
 // 6
 // ............................................................................................
+// //Rotate the given array 'a' by k steps, where k is non-negative.
+// //Note: k can be greater then n as well where n is the size of array 'a'.
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int arr[] = {1,2,3,4,5};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     int ans_arr[size];
+//     int idx = 0;
+//     int k = 2;
+//     // k can be greater then size
+//     k=k%size;
+    
+//     for(int i=size-k; i<size; i++) {
+//         ans_arr[idx++] = arr[i];
+//     }
+    
+//     for(int i=0; i<=k; i++) {
+//         ans_arr[idx++] = arr[i];
+//     }
+    
+//     for(int i=0; i<size; i++) {
+//         cout << ans_arr[i] << " ";
+//     }
+// }
+
+// output:
+// 4 5 1 2 3
 // ............................................................................................
+// //Rotate the given array 'a' by k steps, where k is non-negative.
+// //Note: k can be greater then n as well where n is the size of array 'a'.
+// //(Using vector)
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     vector<int> v;
+//     v.push_back(1);
+//     v.push_back(2);
+//     v.push_back(3);
+//     v.push_back(4);
+//     v.push_back(5);
+    
+//     int k=2;
+//     // k can be greater then size
+//     k=k%v.size();
+    
+//     reverse(v.begin(), v.end());
+//     reverse(v.begin(), v.begin()+k);
+//     reverse(v.begin()+k, v.end());
+    
+//     for(int i=0; i<5; i++) {
+//         cout << v[i] << " ";
+//     }
+// }
+
+// output:
+// 4 5 1 2 3
 // ............................................................................................
+// //Given Q queries, check if the given number is present
+// //in the array or not.
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+    
+//     vector<int> v(n);
+//     for(int i=0; i<n; i++) {
+//         cin >> v[i];
+//     }
+    
+//     const int N = 1e5 + 10;
+//     vector<int> freq(N, 0);
+//     for(int i=0; i<n; i++) {
+//         freq[v[i]]++;
+//     }
+    
+//     cout << "Enter queries : ";
+//     int q;
+//     cin >> q;
+    
+//     while(q--) {
+//         int queryelement;
+//         cin >> queryelement;
+//         cout << freq[queryelement] << endl;
+//     }
+// }
 // ............................................................................................
 
 // ............................................................................................
