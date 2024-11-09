c89e9b241ab1f9630a5734ea9618d8f5b5beb960
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-11-09 20:41:51 +0600

    second commit

--- Functions in One Shot | Revision - 3/main.cpp
+++ Functions in One Shot | Revision - 3/main.cpp
@@ -129,10 +129,140 @@
 // //Given two numbers a and b. write a program using functions 
 // //to print all the odd numbers between them
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int a = 1;
+//     int b = 10;
+
+//     for(int i=a; i<=b; i++) {
+//         if(i%2!=0) {
+//             cout << i << " ";
+//         }
+//     }
+// }
+
+// output:
+// 1 3 5 7 9
+// ............................................................................................
+// //Write a programme to check whether it is prime or not.
+// //prime numbers present (without function)
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+// 	int n;
+// 	cin >> n;
+// 	bool flag = true;
+
+// 	for (int i = 2; i * i <= n; i++) {
+// 		if (n % i == 0) {
+// 			flag = false;
+// 			break;
+// 		}
+// 	}
+
+// 	if (flag) {
+// 		cout << "YES";
+// 	} else {
+// 		cout << "NO";
+// 	}
+// }
 // ............................................................................................
 // //Given two numbers a and b, write a program to print all the prime numbers present 
-// ............................................................................................
 
-// ............................................................................................
+// #include<bits/stdc++.h>
+// using namespace std;
 
+// bool isPrime(int num) {
+//     if (num <= 1) {
+//         return false;  // Handling edge cases
+//     } 
+//     for (int i = 2; i * i <= num; i++) {
+//         if (num % i == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// int main() {
+//     int a=2, b=10;
+//     for(int i=a; i<=b; i++) {
+//         if(isPrime(i)) {
+//             cout << i << " ";
+//         }
+//     }
+// }
+
+// output:
+// 2 3 5 7
 // ............................................................................................
+// //Pass by reference
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void swap(int &x, int &y) {
+//     int temp = x;
+//     x = y;
+//     y = temp;
+// }
+
+// void fun(int *t) {
+//     cout << *t << endl;
+// }
+
+// int main() {
+//     int x = 9;
+//     int c = 1;
+//     int &y = x;
+    
+//     cout << x << " " << y << endl;  // 9 9
+
+//     y = 88;
+//     cout << x << " " << y << endl;  // 88 88
+
+//     swap(x, c);
+//     cout << x << " " << c << endl;  // 1 88
+
+//     int t = 9;
+//     fun(&t);  // 9
+// }
+// ............................................................................................
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void decrease(int n1, int n2) {
+// 	n1--;
+// 	n2 = n2 - 2;
+// 	cout << (n1) << ":" << (n2) << endl;  // 25:11
+// }
+
+// int main() {
+
+// 	int p = 26;
+// 	int q = 13;
+// 	decrease(p, q);
+// 	cout << (p) << ":" << (q);  // 26:13
+// }
+
+// output:
+// 25:11
+// 26:13
+// ............................................................................................
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void makeTwice(int p) {
+// 	p = p * 2;
+// }
+
+// int main() {
+
+// 	int p = 24;
+// 	makeTwice(p);  // The value of p is 24
+// }
