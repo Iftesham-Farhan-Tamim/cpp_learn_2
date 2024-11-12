d6d313c22eebac58d781821003bfb762ae876f4c
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-11-12 21:48:35 +0600

    second commit

--- Prefix Sum Problems | Problems in Arrays - 3/main.cpp
+++ Prefix Sum Problems | Problems in Arrays - 3/main.cpp
@@ -131,3 +131,73 @@
 // output:
 // 1
 // ............................................................................................
+// //Given an array of integer of size n. Answer q queries where
+// //you need to print the sum of values in a given range of indices
+// //from l to r (both included).
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // index -> 0 1 2 3 4 5 6
+// // vector -> 0 x1 x2
+
+// // 0 based indexing -> 0 ..... n-1
+// // 1 based indexing -> 1 ..... n
+
+// // 5 1 2 3 4
+// // 0 5 1 2 3 4
+// // 0 5 6 8 11 15 -> prefix sum array
+// // l = 1, r = 3
+// // ans = v[r] - v[l-1] = v[3] - v[0] = 8-0 = 8
+
+// // l = 2, r = 5
+// // ans = v[r] - v[l-1] = v[5] - v[1] = 15-5 = 10
+
+// int main() {
+//     int n;
+//      cin >> n;
+
+//      vector<int> v(n+1, 0);
+//      for(int i=1; i<=n; i++) {
+//         cin >> v[i];
+//      }
+
+//      //calculate prefix sum array
+//      for(int i=0; i<=n; i++) {
+//         v[i]+=v[i-1];
+//      }
+
+//      int q;
+//      cin >> q;
+
+//      while(q--) {
+//         int l, r;
+//         cin >> l >> r;
+
+//         int ans = 0;
+//         // ans = prefixsumarray[r] - prefixsumarray[l-1]
+//         ans = v[r] - v[l-1];
+//         cout << ans << endl;
+//      }
+// }
+
+// output:
+// 5
+// 5 1 2 3 4
+// 3
+// 1 3
+
+// output:
+// 8
+
+// input:
+// 2 5
+
+// output:
+// 10
+
+// input:
+// 4 4
+
+// output:
+// 3
