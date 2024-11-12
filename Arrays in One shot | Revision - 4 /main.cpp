26127588b658ca8c03489ead9475dbeddba8dc47
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-11-13 03:14:50 +0600

    second commit

---
+++ Arrays in One shot | Revision - 4 /main.cpp
@@ -0,0 +1,47 @@
+// //Given two vectors arr1[] ans arr2[] of size m and n sorted in increasing order.
+// //Merge the two arrays into a single sorted array of size m+n.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+       
+}
+
+// ............................................................................................
+// //Given a vector arr[] sorted in increasing order of size n and an integer x.
+// //find if there exists a pair in the array whose sum is exactly x.
+
+
+// ............................................................................................
+// //Given a vector arr[] sorted in increasing order of n size and an integer x, 
+// //find if there exists a pair in the array whose absolute difference is exactly x.
+
+
+// ............................................................................................
+// //Given a vector arr[] sorted in increasing order. Return an array of squares
+// //of each number sorted in increasing order, where size of vector 1<size<101.
+
+
+// ............................................................................................
+// //Given a vector arr[] sorted in increasing order of n size and an integer x.
+// //find the number of unique pairs that exist in the array whose absolute sum is exactly x.
+
+
+// ............................................................................................
+// //Given a vector array nums, print the count of triplets [nums[i], nums[j], nums[k]]
+// //such that i!=j, i!=k and j!=k, and nums[i] + nums[j] + nums[k] ==x. where k is an
+// //integer given by the user.
+
+
+// ............................................................................................
+
+// ............................................................................................
+
+// ............................................................................................
+
+// ............................................................................................
+
+// ............................................................................................
+
+// ............................................................................................
