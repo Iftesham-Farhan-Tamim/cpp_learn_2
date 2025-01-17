0fa3ce509b624cf3f7b13d8946d23359aaadf5b2
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-01-18 02:27:57 +0600

    final

--- Advanced Problem Solving in 2D Arrays - 3/main.cpp
+++ Advanced Problem Solving in 2D Arrays - 3/main.cpp
@@ -49,7 +49,7 @@
 // 1 2 3 4 
 // 5 6 7 8 
 // 9 10 11 12 
-// 34
+// sum: 34
 // ............................................................................................
 // // Method-2: Pre-Calculating the horizontal sum for each row in the matrix
 
@@ -117,68 +117,81 @@
 // ............................................................................................
 // // Method-3: Prefix sum over columns and Rows Both
 
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int rectangleSum(vector<vector<int> > &matrix, int l1, int r1, int l2, int r2) {
-    int sum=0;
+// int rectangleSum(vector<vector<int> > &matrix, int l1, int r1, int l2, int r2) {
+//     int sum=0;
 
-    // prefix sum array row-wise
-    for(int i=0; i<matrix.size(); i++) {
-        for(int j=1; j<matrix[0].size(); j++) {
-            matrix[i][j]+=matrix[i][j-1];
-        }
-    }
+//     // prefix sum array row-wise
+//     for(int i=0; i<matrix.size(); i++) {
+//         for(int j=1; j<matrix[0].size(); j++) {
+//             matrix[i][j]+=matrix[i][j-1];
+//         }
+//     }
 
-    // prefix sum array column-wise
-    for(int i=1; i<matrix.size(); i++) {
-        for(int j=0; j<matrix[0].size(); j++) {
-            matrix[i][j]+=matrix[i-1][j];
-        }
-    }
+//     // prefix sum array column-wise
+//     for(int i=1; i<matrix.size(); i++) {
+//         for(int j=0; j<matrix[0].size(); j++) {
+//             matrix[i][j]+=matrix[i-1][j];
+//         }
+//     }
 
-    //printing prefix sum 2d array
-    for(int i=0; i<matrix.size(); i++) {
-        for(int j=0; j<matrix[0].size(); j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+//     //printing prefix sum 2d array
+//     for(int i=0; i<matrix.size(); i++) {
+//         for(int j=0; j<matrix[0].size(); j++) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
 
-    int top_sum=0; 
-    int left_sum=0; 
-    int topleft_sum = 0;
-    if(l1!=0) top_sum = matrix[l1-1][r2];
-    if(r1!=0) left_sum = matrix[l2][r2-1];
-    if(l1!=0 && r1!=0) topleft_sum = matrix[l1-1][r1-1];
+//     int top_sum=0; 
+//     int left_sum=0; 
+//     int topleft_sum = 0;
+//     if(l1!=0) top_sum = matrix[l1-1][r2];
+//     if(r1!=0) left_sum = matrix[l2][r1-1];
+//     if(l1!=0 && r1!=0) topleft_sum = matrix[l1-1][r1-1];
 
-    sum = matrix[l2][r2] - top_sum - left_sum + topleft_sum;
-    return sum;
-}
+//     sum = matrix[l2][r2] - top_sum - left_sum + topleft_sum;
+//     return sum;
+// }
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int> > matrix(n, vector<int> (m));
+// int main() {
+//     int n, m;
+//     cin >> n >> m;
+//     vector<vector<int> > matrix(n, vector<int> (m));
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            cin >> matrix[i][j];
-        }
-    }
+//     for(int i=0; i<n; i++) {
+//         for(int j=0; j<m; j++) {
+//             cin >> matrix[i][j];
+//         }
+//     }
 
-    int l1, r1, l2, r2;
-    cin >> l1 >> r1 >> l2 >> r2;
+//     int l1, r1, l2, r2;
+//     cin >> l1 >> r1 >> l2 >> r2;
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+//     for(int i=0; i<n; i++) {
+//         for(int j=0; j<m; j++) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
 
-    int sum = rectangleSum(matrix, l1, r1, l2, r2);
-    cout << "sum: " << sum << endl;
-}
+//     int sum = rectangleSum(matrix, l1, r1, l2, r2);
+//     cout << "sum: " << sum << endl;
+// }
 
-// ............................................................................................
+// input:
+// 3 4
+// 1 2 3 4 5 6 7 8 9 10 11 12
+// 1 1
+// 2 2
+
+// input:
+// 1 2 3 4 
+// 5 6 7 8 
+// 9 10 11 12 
+// 1 3 6 10 
+// 6 14 24 36 
+// 15 33 54 78 
+// sum: 34
